35f45d2a2bf55b29681a603f66528077f5b4fbbd
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-08-19 23:07:01 -0300

    add products fake data

---
+++ src/data/products.ts
@@ -0,0 +1,14 @@
+export const DATA_PRODUCTS = [
+  {
+    id: 'comp',
+    name: 'computer',
+  },
+  {
+    id: 'key-99',
+    name: 'keyboard',
+  },
+  {
+    id: 'cam-10',
+    name: 'camera',
+  },
+];
