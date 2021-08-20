64dc54a7549587c622c44dff0f6876b353c2b7f3
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-08-19 22:56:37 -0300

    add Routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,26 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import * as Routes from './constants/routes';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={Routes.HOME}>
+          <h1>Seja bem vindo ao nosso website!</h1>
+        </Route>
+
+        <Route exact path={Routes.PRODUCTS}>
+          <h1>Olá, eu sou a página de produtos</h1>
+        </Route>
+
+        <Route exact path={Routes.PRODUCT}>
+          <h1>Olá, veja aqui o detalhe desse produto</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
